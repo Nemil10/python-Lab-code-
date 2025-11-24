dd32e515a6e4c0ea5c4cf6f30abafb4256b75087
Nemil10 <nemilchhagani@gmail.com>
2025-11-24 21:15:32 +0530

    Add files via upload

---
+++ lab 1/lab1--1.py
@@ -0,0 +1,12 @@
+for i in range(5):
+    print("Welcome to Python")
+    
+    
+
+print("Number\tSquare\tCube")
+for num in range(1, 6):
+    print(f"{num}\t{num**2}\t{num**3}")
+
+
+result = (9.5*4.5-2.5*3)/(45.5-3.5)
+print("result=",result)
